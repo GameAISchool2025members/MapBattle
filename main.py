fc69c916e70deaee8a13c859c1b464f66ad9e53b
Liam Snow <lasnow92@gmail.com>
2025-06-26 23:46:28 +0100

    pygame_gui
    Started integrating pygame_gui into the project.

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 import pygame
-
-import battle_phase
-import end_phase
-import menu
+import pygame_gui
 import game_state_manager
+import menu
 import game_init
 import pre_phase
+import battle_phase
+import end_phase
 
 # pygame setup
 pygame.init()
 pygame.display.set_caption("Map Battle")
+resolution = (1280, 720)
+screen = pygame.display.set_mode(resolution, pygame.SCALED)
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
 gameStateManager = game_state_manager.GameStateManager()
-menu = menu.Menu()
-menu.__init__()
+uiManager = pygame_gui.UIManager(resolution)
+menu = menu.Menu(screen)
 
 init_data = game_init.setup_battle(8, 12, 3)
-
 first_run = True
 
-
-
 while running:
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        uiManager.process_events(event)
+
     match gameStateManager.get_state():
         case game_state_manager.GameState.MENU:
-            menu.display_menu(dt)
+            menu.display_menu()
         case game_state_manager.GameState.PRE_PHASE:
             pre_phase.run(init_data, first_run)
         case game_state_manager.GameState.BATTLE_PHASE:
@@ -43,9 +49,8 @@ while running:
         case game_state_manager.GameState.QUIT:
             running = False
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+    uiManager.update(dt)
+    uiManager.draw_ui(screen)
+    pygame.display.update()
 
 pygame.quit()
--- menu.py
+++ menu.py
@@ -1,26 +1,23 @@
 import pygame
+import pygame_gui
 
 class Menu:
-    def __init__(self):
+    def __init__(self, screen):
         if not pygame.font:
             print("Warning, fonts disabled")
 
-        self.screen = pygame.display.set_mode((1280, 720), pygame.SCALED)
+        self.screen = screen
         self.background = pygame.Surface(self.screen.get_size())
         self.background = self.background.convert()
-        self.background.fill((170, 238, 187))
+        self.background.fill((170, 238, 187)) # Green
 
-    def display_menu(self, dt):
-        # fill the screen with a color to wipe away anything from last frame
+    def display_menu(self):
         self.display_title_text()
         self.screen.blit(self.background, (0, 0))
-
-        # flip() the display to put your work on screen
-        pygame.display.flip()
 
     def display_title_text(self):
         if pygame.font:
             font = pygame.font.Font(None, 64)
-            text = font.render("Map Battle", True, (0, 0, 0))
+            text = font.render("Map Battle", True, (0, 0, 0)) # Black
             text_pos = text.get_rect(centerx=self.background.get_width()/2, y=10)
             self.background.blit(text, text_pos)
