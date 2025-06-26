1e1f93f2ec511a06ef5524e6c6c148a0583bdad1
Aji <aji.daleni@rendertools.se>
2025-06-27 01:20:33 +0200

    hello

--- pre_phase.py
+++ pre_phase.py
@@ -1,4 +1,16 @@
 import battle
 
-def run(init_data, first_run):
-    battle.Battle(init_data)
+import evolution
+import game_init
+import game_state_manager
+def run(init_data: game_init.BattleState, run_counter: int):
+    if run_counter:
+        evolution.RunEvolution(init_data.UnitsAgentA,
+                               init_data.UnitsAgentB,
+                               31 + run_counter,
+                               init_data.GameGrid,
+                               69,
+                               5,
+                               0
+                               )
+    game_state_manager.set
