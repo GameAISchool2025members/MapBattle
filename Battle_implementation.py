0fabae442954b9e8024cd2c05a7345deb14485cc
Andreas <Andreas.Jonasson1993@gmail.com>
2025-06-26 17:00:55 +0200

    Fixed error

--- Battle_implementation.py
+++ Battle_implementation.py
@@ -1,23 +1,59 @@
 from data_structs import UnitStat, Grid, ResultOfBattle, Action
-from typing import List
+from typing import List, Tuple
+import math as math
 
 def Internal_Battle( 
-    UnitsForAgentA: List[UnitStat],
-    UnitsForAgentB: List[UnitStat],
-    MapGrid: Grid
-        ) -> ResultOfBattle:
+        UnitsForAgentA: List[UnitStat],
+        UnitsForAgentB: List[UnitStat],
+        MapGrid: Grid
+    ) -> ResultOfBattle:
     
 
 
     
     return None
+
+def ManhattanDistanceBetweenPoints(
+        PosA: Tuple[int, int],
+        PosB: Tuple[int, int]
+    ) -> int:
+
+    return abs(PosA[0] - PosB[0]) + abs(PosA[1] - PosB[1])
+
+def ApplyAction(
+        AllUnits: List[UnitStat],
+        MapGrid: Grid,
+        ActionToApply: Action
+    ) -> Tuple[List[UnitStat], Grid]:
+
+    if ActionToApply.Type == ActionToApply.Type.Move:
+        return ApplyMoveAction(
+            AllUnits,
+            MapGrid,
+            ActionToApply
+        )
+    elif ActionToApply.Type == ActionToApply.Type.Attack:
+        return ApplyAttackAction(
+            AllUnits,
+            MapGrid,
+            ActionToApply
+        )
+
+
+    return Tuple(AllUnits, MapGrid)
 
 
 
 def DecideOnAction(
-    UnitTakingAction: UnitStat,
-    EnemyUnits: List[UnitStat],
-    MapGrid: Grid
-        ) -> Action:
+        UnitTakingAction: UnitStat,
+        EnemyUnits: List[UnitStat],
+        MapGrid: Grid
+    ) -> Action:
     
+
+
     return None
+
+
+if __name__ == "__main__":
+    print("Starting Battle_implementation.py")
