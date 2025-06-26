a965936ed61691129c5fecd80d4ab18432309d39
Liam Snow <lasnow92@gmail.com>
2025-06-26 22:40:35 +0100

    GameStateManager
    Added a basic manager to handle the game state. This is to be referenced from other classes to control the main game state, which is checked in the main loop.

---
+++ game_state_manager.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class GameState(Enum):
+    MENU = 1
+    PRE_PHASE = 2
+    BATTLE_PHASE = 3
+    END_PHASE = 4
+    QUIT = 5
+
+class GameStateManager:
+    def __init__(self):
+        self.state = GameState.MENU
+
+    def get_state(self):
+        return self.state
+
+    def set_state(self, state):
+        self.state = state
